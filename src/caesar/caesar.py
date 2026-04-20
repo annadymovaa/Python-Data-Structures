9f51c01866b0f47cf93339f926b9f2a766a15226
annadymovaa <aanadumova@hse.edu.ru>
2026-04-20 23:33:49 +0500

    improved var names and exception

--- src/caesar/caesar.py
+++ src/caesar/caesar.py
@@ -9,22 +9,22 @@ def get_letter(mode: str, alphabet: str, letter: str, shift: int) -> str:
     new_pos = shifted % len(alphabet)
     return alphabet[new_pos]
 
-def cipher(mode: str, phrase: str, shift: int):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def cipher(mode: str, phrase: str, shift: int) -> None:
+    lowercase_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    uppercase_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     symbols = ' !@#$%^&*()_+-=[]\{\}|:;\'"<>,.?/~1234567890'
 
     new_phrase = ''
     for letter in phrase:
-        if letter in alphabet:
-            new_phrase += get_letter(mode, alphabet, letter, shift)
-        elif letter in alpha:
-            new_phrase += get_letter(mode, alpha, letter, shift)
+        if letter in lowercase_alphabet:
+            new_phrase += get_letter(mode, lowercase_alphabet, letter, shift)
+        elif letter in uppercase_alphabet:
+            new_phrase += get_letter(mode, uppercase_alphabet, letter, shift)
         elif letter in symbols:
             new_phrase += letter
         else:
-            raise Exception("The script does not support your language yet.")
-            break
+            raise ValueError("The script does not support your language yet.")
+
     print(new_phrase)
 
 if __name__ == "__main__":
