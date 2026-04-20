8175d61e520edcd9c94befb4c1632f4838b01270
annadymovaa <aanadumova@hse.edu.ru>
2026-04-20 23:44:16 +0500

    changed  the mode logic

--- src/caesar/caesar.py
+++ src/caesar/caesar.py
@@ -1,34 +1,39 @@
 import sys
 
-def get_letter(mode: str, alphabet: str, letter: str, shift: int) -> str:
+def get_letter(alphabet: str, letter: str, shift: int) -> str:
     pos = alphabet.index(letter)
-    if mode == 'encode':
-        shifted = pos + shift
-    else:
-        shifted = pos - shift
+    shifted = pos + shift
     new_pos = shifted % len(alphabet)
     return alphabet[new_pos]
 
-def cipher(mode: str, phrase: str, shift: int) -> None:
+def cipher(phrase: str, shift: int) -> None:
     lowercase_alphabet = 'abcdefghijklmnopqrstuvwxyz'
     uppercase_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    symbols = ' !@#$%^&*()_+-=[]\{\}|:;\'"<>,.?/~1234567890'
+    #symbols = ' !@#$%^&*()_+-=[]\{\}|:;\'"<>,.?/~1234567890'
 
     new_phrase = ''
     for letter in phrase:
         if letter in lowercase_alphabet:
-            new_phrase += get_letter(mode, lowercase_alphabet, letter, shift)
+            new_phrase += get_letter(lowercase_alphabet, letter, shift)
         elif letter in uppercase_alphabet:
-            new_phrase += get_letter(mode, uppercase_alphabet, letter, shift)
-        elif letter in symbols:
+            new_phrase += get_letter(uppercase_alphabet, letter, shift)
+        elif letter.isalpha() == False:
             new_phrase += letter
         else:
             raise ValueError("The script does not support your language yet.")
 
-    print(new_phrase)
+    return new_phrase
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
-        cipher(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+        mode = sys.argv[1]
+        phrase = sys.argv[2]
+        shift = int(sys.argv[3])
+
+        if mode == 'decode':
+            shift *= -1
+
+        result = cipher(phrase, shift)
+        print(result)
     else:
-        raise Exception('Wrong number of arguments!')
+        raise IndexError('Wrong number of arguments!')
