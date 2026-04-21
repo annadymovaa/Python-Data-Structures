cd8fefd6704d5086f182770b611dd9570086dc5c
annadymovaa <aanadumova@hse.edu.ru>
2026-04-21 12:02:22 +0500

    changed var naming

--- src/emails/names_extractor.py
+++ src/emails/names_extractor.py
@@ -1,16 +1,16 @@
 import sys
 
 def get_emails(filepath: str) -> list:
-    with open(filepath, 'r', encoding='utf-8')as file:
+    with open(filepath, 'r', encoding='utf-8') as file:
         emails = file.readlines()
     
     employees = [['Name', 'Surname', 'E-mail']]
     for email in emails:
         email = email.replace('\n', '')
-        info = email.split('@')
-        person = info[0].split('.')
-        name = person[0].capitalize()
-        surname = person[1].capitalize()
+        parts = email.split('@')
+        name_parts = parts[0].split('.')
+        name = name_parts[0].capitalize()
+        surname = name_parts[1].capitalize()
         employees.append([name, surname, email])
     return employees
 
